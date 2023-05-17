93415a15ef6779fff108392132e4361142e68d84
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-05-17 11:29:37 +0200

    Cloudinary

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const User = require('../models/User.model')
 const { isLoggedIn, checkRoles } = require('../middlewares/route-guard')
+const uploaderMiddleware = require('../middlewares/uploader.middleware')
 
 // User profile
 router.get('/profile', isLoggedIn, (req, res, next) => {
@@ -45,18 +46,31 @@ router.get('/edit/:_id', (req, res, next) => {
 })
 
 // edit user form (handler) - PROTECTED
-router.post('/edit/:_id', (req, res, next) => {
+router.post('/edit/:_id', uploaderMiddleware.single('avatar'), (req, res, next) => {
 
-    const { username, email, avatar, description } = req.body
+
+    const { username, email, description } = req.body
     const { _id } = req.params
 
-
-    User
-        .findByIdAndUpdate(_id, { username, email, avatar, description })
-        // .then(user => res.send(user))
-        .then(() => res.redirect(`/users/details/${_id}`))
-        .catch(err => next(err))
+    if (req.file) {
+        const { path: avatar } = req.file
+        User
+            .findByIdAndUpdate(_id, { username, email, description, avatar })
+            // .then(user => res.send(user))
+            .then(() => res.redirect(`/users/details/${_id}`))
+            .catch(err => next(err))
+    } else {
+        User
+            .findByIdAndUpdate(_id, { username, email, description })
+            // .then(user => res.send(user))
+            .then(() => res.redirect(`/users/details/${_id}`))
+            .catch(err => next(err))
+    }
 })
+
+
+
+
 
 // delete book (de tipo POST!!!!) - PROTECTED
 router.post('/delete/:_id', (req, res, next) => {
